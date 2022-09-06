770e1e0a8a02ed8133cf0026615a0d66d6c75f7c
Stefanomarton <sstefanomarton@gmail.com>
2022-09-06 20:09:34 +0200

    added yay script

--- setup.sh
+++ setup.sh
@@ -2,4 +2,7 @@
 sudo pacman -S ansible ansible-core &&
 ansible-galaxy collection install community.general &&
 sudo ansible-pull -U https://github.com/Stefanomarton/SMABS.git &&
-echo "Agg' fatto"
+echo "Agg' fatto con paccheman ed ansibble"
+git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+echo "Agg' installlat yay"
+yay -S ferdium nerd-font-complete ptsh ncspot teams-for-linux update-grub ttf-ms-win11-auto simple-usb-automount dracula-cursors-git dracula-gtk-theme-git dracula-icons-git
