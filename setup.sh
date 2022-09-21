378b7b54b5888c6ec26c89e0c1653d79c727461c
Stefanomarton <sstefanomarton@gmail.com>
2022-09-21 21:44:36 +0200

    updates

--- setup.sh
+++ setup.sh
@@ -5,4 +5,4 @@ sudo ansible-pull -U https://github.com/Stefanomarton/SMABS.git &&
 echo "Agg' fatto con paccheman ed ansibble"
 git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 echo "Agg' installlat yay"
-yay -S ferdium nerd-font-complete ptsh ncspot teams-for-linux update-grub ttf-ms-win11-auto simple-usb-automount dracula-cursors-git dracula-gtk-theme-git dracula-icons-git
+yay -S ferdium nerd-font-complete ptsh ncspot teams-for-linux update-grub ttf-ms-win11-auto simple-usb-automount dracula-cursors-git dracula-gtk-theme-git dracula-icons-git youtube-music-bin
