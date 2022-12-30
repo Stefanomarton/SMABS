d008533769ae3651c1390a8eb4a471051d558595
Stefanomarton <sstefanomarton@gmail.com>
2022-12-30 23:22:07 +0100

    added packages

--- setup.sh
+++ setup.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+# Run using $ curl https://raw.githubusercontent.com/Stefanomarton/SMABS/master/setup.sh | sh
 
-# Run using
-# $ curl https://raw.githubusercontent.com/Stefanomarton/SMABS/master/setup.sh | sh
+echo 'Updating Applications'
+sudo pacman -Syyu
+echo ' '
+
+echo 'Cleaning caches and directories'
+sudo pacman -Sc 
+echo ' '
+
+echo ' '
+echo 'PreInstalling process completed'
+
 
 # Yay Installing
 pacman -S --needed git base-devel
@@ -9,14 +19,86 @@ git clone https://aur.archlinux.org/yay.git
 cd yay
 sudo chown -R stefanomarton:users yay
 makepkg -si
+cd
 echo "Agg' installlat yay"
 
 pacman=(
-    nvim
+    neovim 
+		zsh
+		obsidian
+		kitty
+		ranger
+		discord
+		firefox
+		code
+		pcmanfm
+		wofi
+		waybar
+		bitwarden
+		code
+		discord
+		emacs
+		feh
+		flameshot
+		fzf
+		git-delta
+		lazygit
+		bat
+		blueberry
+		picom
+		glow
+		gparted
+		gpick
+		helix
+		stow
+    pipewire-alsa
+    pipewire-jack
+    pipewire-pulse
+		rofi
+		tectonic
+		qmk
+    zathura
+    zathura-pdf-mupdf
+		sxiv
+		btop
+		darktable
+		pavucontrol
+    python-pillow
+    python-pip
+    python-pyalsa
+    python-typer
+		ripgrep
+		starship
+		xclip
+    thunderbird
 		)
 
 yay=(
+		ptsh
+		anki
 		spotify
+		cava
+		insync
+		ferdium
+		swww
+		espanso
+		git-credential-manager-core
+		moar
+    perl-eval-closure
+    perl-file-homedir
+    perl-log-dispatch
+    perl-log-log4perl
+    perl-namespace-autoclean
+    perl-params-validationcompiler
+    perl-perl-minimumversion
+    perl-specio
+    perl-try-tiny
+    perl-unicode-linebreak
+    perl-yaml-tiny
+    zaread-git
+		ncspot
+    ticktick
+		topgrade
 		)
 
 # Installing packages, use pacman -Qqe > pkglist.txt to create the list
@@ -25,10 +107,11 @@ sudo pacman -S --noconfirm ${pacman[@]}
 yay -S --noconfirm ${yay[@]}
 
 # DotFiles 
-# cd ..
-# git clone --recursive https://github.com/Stefanomarton/DotFiles.git && cd DotFiles && stow . 
-#
-# # Fonts configuration
-# sudo cp -r ~/Media/Fonts/JetBrainsMono/ /usr/share/fonts/
+git clone --recursive https://github.com/Stefanomarton/DotFiles.git && cd DotFiles && stow . 
 
+# Fonts configuration
+sudo cp -r ~/Media/Fonts/JetBrainsMono/ /usr/share/fonts/
 
+# Git Credential
+git-credential-manager configure
+git config --global credential.credentialStore cache
