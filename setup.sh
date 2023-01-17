1cf09e56c7e276f11990937f91e5d68b2db24949
Stefanomarton <sstefanomarton@gmail.com>
2023-01-18 00:06:47 +0100

    fix: some minor error

--- setup.sh
+++ setup.sh
@@ -2,86 +2,88 @@
 # Run using $ curl https://raw.githubusercontent.com/Stefanomarton/SMABS/master/setup.sh | sh
 
 echo 'Updating Applications'
-sudo pacman -Syyu
+sudo pacman -Syu
 
 echo 'Cleaning caches and directories'
-sudo pacman -Sc 
-
-echo 'PreInstalling process completed'
+sudo pacman -Sc
 
 echo 'Installing yay'
 # Yay Installing
-pacman -S git base-devel
+sudo pacman -S git base-devel
 git clone https://aur.archlinux.org/yay.git
-sudo chown -R stefanomarton:user yay
+sudo chown -R stefanomarton:stefanomarton yay
 cd yay
 makepkg -si
 cd
 echo 'Installed yay'
 
 pacman=(
-    neovim 
-		zsh
-		obsidian
-		kitty
-		ranger
-		discord
-		firefox
-		code
-		pcmanfm
-		wofi
-		waybar
-		bitwarden
-		code
-		discord
-		emacs
-		feh
-		flameshot
-		fzf
-		git-delta
-		lazygit
-		bat
-		blueberry
-		picom
-		glow
-		gparted
-		gpick
-		helix
-		stow
+    neovim
+    zsh
+    obsidian
+    kitty
+    ranger
+    discord
+    firefox
+    code
+    pcmanfm
+    wofi
+    waybar
+    bitwarden
+    code
+    discord
+    emacs
+    feh
+    flameshot
+    fzf
+    git-delta
+    lazygit
+    bat
+    blueberry
+    picom
+    glow
+    gparted
+    gpick
+    helix
+    stow
     pipewire-alsa
     pipewire-jack
     pipewire-pulse
-		rofi
-		tectonic
-		qmk
+    rofi
+    tectonic
+    qmk
     zathura
     zathura-pdf-mupdf
-		sxiv
-		btop
-		darktable
-		pavucontrol
+    sxiv
+    btop
+    darktable
+    pavucontrol
     python-pillow
     python-pip
     python-pyalsa
     python-typer
-		ripgrep
-		starship
-		xclip
+    ripgrep
+    starship
+    xclip
     thunderbird
-		npm
-		)
+    npm
+    redshift
+    noto-fonts-emoji
+    xorg
+    xorg-xsetroot
+    hugo
+)
 
 yay=(
-		ptsh
-		anki
-		spotify
-		cava
-		insync
-		ferdium
-		swww
-		espanso
-		git-credential-manager-core
-		moar
+    ptsh
+    cava
+    insync
+    ferdium
+    swww
+    espanso
+    # git-credential-manager-core
+    github-cli
+    moar
     perl-eval-closure
     perl-file-homedir
     perl-log-dispatch
@@ -94,25 +96,30 @@ yay=(
     perl-unicode-linebreak
     perl-yaml-tiny
     zaread-git
-		ncspot
+    ncspot
     ticktick
-		topgrade
-		)
+    topgrade
+)
 
 # Installing packages, use pacman -Qqe > pkglist.txt to create the list
 
 sudo pacman -S --noconfirm ${pacman[@]}
 yay -S ${yay[@]}
 
-# DotFiles 
-git clone --recursive https://github.com/Stefanomarton/DotFiles.git && cd DotFiles && stow . 
+sudo pacman -Rns grml-zsh-config
+
+# DotFiles
+git clone --recursive https://github.com/Stefanomarton/DotFiles.git && cd DotFiles && stow .
 
 # Fonts configuration
 sudo cp -r ~/Media/Fonts/JetBrainsMono/ /usr/share/fonts/
 
-# Git Credential
-git-credential-manager configure
-git config --global credential.credentialStore cache
-
 # Settings zsh as default shell
 chsh -s /bin/zsh stefanomarton
+
+# Install zpico
+curl -sL --create-dirs https://gitlab.com/thornjad/zpico/-/raw/main/zpico.zsh -o $HOME/.local/lib/zpico/zpico.zsh
+
+# Git Credential
+gh auth login
+gh auth git-credential
