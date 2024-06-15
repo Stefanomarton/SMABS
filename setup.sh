347c2e52dec38d1eaf22c6559d43d1fd0caea919
Stefanomarton <sstefanomarton@gmail.com>
2024-06-15 19:03:25 +0200

    feat: update to paru and clean it all

--- setup.sh
+++ setup.sh
@@ -7,198 +7,188 @@ sudo pacman -Syu
 echo 'Cleaning caches and directories'
 sudo pacman -Sc
 
-echo 'Installing yay'
-# Yay Installing
+echo 'Installing paru'
+
 sudo pacman -S git base-devel
-git clone https://aur.archlinux.org/yay.git
-sudo chown -R $USER:$USER yay
-cd yay
+git clone https://aur.archlinux.org/paru.git
+sudo chown -R $USER:$USER paru
+cd paru
 makepkg -si
 cd
-echo 'Installed yay'
-
-# Prompt user for selection
-echo "Select an option for environment:"
-echo "1: Xorg"
-echo "2: Wayland"
-echo "3: Both"
-
-read -p "Enter your choice (1/2/3): " choice
+echo 'Installed paru'
 
 ###############################
 # Defining packages variables #
 ###############################
 
-yay=()
-pacman=()
-
-programs_yay=(
-		cava
-		github-cli
-		insync
-		moar
-		zaread-git
-		ncspot
-		spotify
-		ticktick
-		topgrade
-		update-grub
-		texlive-meta
-		looking-glass-git
-		ptsh
-		wpgtk
-)
-programs_pacman=(
-        stow
-        rocm-smi-lib
-        atool
-        perl-image-exiftool
-        words
-        yazi
-        navi
-		cargo
-		neovim
-        biber
-		zsh
-		zoxide
-		obsidian
-		kitty
-		discord
-		firefox
-		code
-		nemo
-		bitwarden
-		discord
-		emacs-nativecomp
-		fzf
-		syncthing
-		git-delta
-		lazygit
-		bat
-		bluez
-        blueman
-		glow
-		gparted
-		gpick
-        fd
-        libqalculate
-		pipewire-alsa
-        libreoffice-fresh
-		pipewire-pulse
-		rofi
-		qmk
-		zathura
-		zathura-pdf-mupdf
-		sxiv
-		btop
-		darktable
-		pavucontrol
-		python-pillow
-		python-pip
-		python-pyalsa
-		python-typer
-		ripgrep
-		thunderbird
-		npm
-		redshift
-		noto-fonts-emoji
-		hugo
-		networkmanager
-		gnome-keyring
-		linux-zen
-        enchant
-		pandoc
-		deja-dup
-		feh
-		flameshot
-		hunspell-en_us
-		hunspell-en_gb
-		hunspell-en_it
-		languagetool
-		libre-office-fresh
-		spice-vdagent
-		texlive-meta
-		virt-manager
-		picom
-		polkit-gnome
-		qemu
-        # Music and Video
-		mpv
-		cmus
-        spotify-launcher
-        # Archives
-		unzip
-        unrar
-        # Language-Servers
-        bash-language-server
-		prettier
-)
-
-xorg_yay=(
-        espanso
-	    xbanish
-)
-xorg_pacman=(
-	    xorg
-	    xclip
-	    lxappearance
-)
-
-wl_yay=(
-        tofi
-        # swww
-        wlr-randr
-        espanso-wayland-git
-)
-wl_pacman=(
-        waybar
-    	wl-clipboard
-        slurp
-        grim
-)
-
-cargo=(
-		wallust
-		)
-
-if [ "$choice" -eq 1 ]; then
-    yay=("${xorg_yay[*]}" "${programs_yay[*]}")
-    pacman=("${xorg_pacman}[*]" "${programs_pacman[*]}")
-
-elif [ "$choice" -eq 2 ]; then
-    yay=("${wl_yay[*]}" "${programs_yay[*]}") 
-    pacman=("${wl_pacman}[*]" "${programs_pacman[*]}")
-
-elif [ "$choice" -eq 3 ]; then
-    merged_yay=("${wl_yay[*]}" "${_wlpacman[*]}" "${programs_yay[*]}")
-    merged_pacman=("${wl_pacman[*]}" "${xorg_pacman[*]}" "${programs_pacman[*]}")
-    yay="${merged_yay}" 
-    pacman="${merged_pacman}" 
-else
-    echo "Invalid choice. Please select a valid option."
-    exit 1
+# check cpu
+vendor=$(lscpu | awk '/Vendor ID/{print $3}')
+if [[ "$vendor" == "GenuineIntel" ]]; then
+    cpu='intel-ucode'
+elif [[ "$vendor" == "AuthenticAMD" ]]; then
+    cpu='amd-ucode'
 fi
 
-# Installin pacman packages
-echo "Installing Pacman Packages"
-for pac in "${pacman[@]}"; do
-		echo "INSTALLING: ${pac}"
-		sudo pacman -S "$pac" --noconfirm --needed
-done
+programs=(
+hyprland
+xdg-desktop-porta-hyprland
+jq
+cava
+buku
+peek
+$cpu
+inkscape
+ollama
+dragon-drop
+xournalpp
+aylurs-gtk-shell
+github-cli
+insync
+moar
+zaread-git
+ncspot
+spotify
+ticktick
+topgrade
+update-grub
+pipewire
+wireplumber
+texlive-meta
+looking-glass-rc
+molsketch
+ptsh
+wpgtk
+stow
+rocm-smi-lib
+atool
+perl-image-exiftool
+words
+yazi
+navi
+cargo
+neovim
+biber
+zsh
+zoxide
+zotero
+obsidian
+kitty
+discord
+floorp-bin
+nemo
+bitwarden
+discord
+emacs-wayland
+fzf
+syncthing
+cliphist
+imv
+git-delta
+lazygit
+bat
+bluez
+blueman
+glow
+gparted
+gpick
+fd
+libqalculate
+pipewire-alsa
+eww-git
+libreoffice-fresh
+pipewire-pulse
+rofi
+qmk
+zathura
+zathura-pdf-mupdf
+sxiv
+btop
+darktable
+pavucontrol
+python-pillow
+pyprland-git
+cmake
+dialog
+dropbox
+dunst
+rust
+rust-analyzer
+btrfs-progs
+python-pip
+python-pyalsa
+python-typer
+ripgrep
+rsync
+ripgrep-all
+thunderbird
+npm
+redshift
+noto-fonts-emoji
+hugo
+networkmanager
+gnome-keyring
+linux-zen
+enchant
+pandoc-cli
+deja-dup
+feh
+hunspell-en_us
+nano
+nmap
+hunspell-it
+languagetool
+libre-office-fresh
+spice-vdagent
+texlive-meta
+virt-manager
+picom
+polkit-gnome
+qemu-base
+qemu-desktop
+# Music and Video
+mpv
+cmus
+spotify-launcher
+# Archives
+unzip
+zip
+unrar
+# Language-Servers
+bash-language-server
+prettier
+tofi-git
+swww
+wlr-randr
+espanso-wayland-git
+socat
+wl-clipboard
+slurp
+nuspell
+nvtop
+opentabletdriver
+steam
+grim
+wallust-git
+wireless_tools
 
-# Installin yay packages
-echo "Installing Yay Packages"
-for ya in "${yay[@]}"; do
-		echo "INSTALLING: ${ya}"
-		yay -S "$ya" --noconfirm --needed
-done
+python-matplotlib
+python-numpy
+python-pandas
+python-pywlroots
+python-rich
 
-# Installin cargo packages
-echo "Installing Cargo Packages"
-for i in "${cargo[@]}"; do
-		echo "INSTALLING ${i} from cargo"
-		cargo install "$i"
-done
+hspell
+spell
+aspell-en
+aspell-it
+
+base-devel
+
+alsa-utils
+)
+
+# enable emacs service
+systemctl --user enable emacs
 
 # Check if grml is installed and removed it
 echo "Check if grml is installed and removed it"
@@ -223,8 +213,8 @@ sudo update-grub
 
 # DotFiles
 echo "Installing DotFiles"
-git clone --recurse-submodules https://github.com/Stefanomarton/DotFiles.git .dotfiles
-cd ~/.dotfiles/ && stow .
+git clone --recurse-submodules https://github.com/Stefanomarton/DotFiles.git ~/.dotfiles/
+stow ~/.dotfiles/ 
 
 # Fonts configuration
 echo "Configuring Fonts"
@@ -241,12 +231,6 @@ curl -sL --create-dirs https://gitlab.com/thornjad/zpico/-/raw/main/zpico.zsh -o
 # Copy zshenv
 echo "Copyng zshenv for proper login config"
 sudo cp -r ~/SMABS/Files/zshenv /etc/zsh
-
-# Installing dmenu
-echo "Installing Dmenu"
-cd /home/$USER/.local/share/dmenu-flexipatch
-make
-sudo make install
 
 # Git Credential
 # gh auth login
